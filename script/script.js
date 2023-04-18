b45304c69975ded45563ddb64e848c8b202ad637
Lal Bahadur Budhachhetri <lalbahadurbc@neputer.com>
2023-04-18 18:29:38 +0545

    initial commit with index page

---
+++ script/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".testimonial__slider").slick({
+    infinite: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+  });
+});
