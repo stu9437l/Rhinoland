e4e5150ac75eb4a695f66321cfa321f2eaf14f40
Lal Bahadur Budhachhetri <lalbahadurbc@neputer.com>
2023-04-20 10:50:14 +0545

    chore: initial commit

--- script/script.js
+++ script/script.js
@@ -1,9 +1,91 @@
 $(document).ready(function () {
+  // our partners slider
+  $(".collection-card__list").slick({
+    arrows: true,
+    dots: false,
+    infinite: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1.1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+        },
+      },
+    ],
+  });
+
+  // testimonial slider
   $(".testimonial__slider").slick({
     infinite: false,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
     dots: false,
+  });
+
+  // our partners slider
+  $(".partner-company__list").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 0,
+    speed: 8000,
+    pauseOnHover: false,
+    cssEase: "linear",
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+
+  //product media images
+
+  $(".product-slider__list").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: false,
+    fade: true,
+    asNavFor: ".product-thumbnail__list",
+  });
+  $(".product-thumbnail__list").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: ".product-slider__list",
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+  });
+
+  //mobile menu dropdown
+  $(".mobile-menu .main-nav__link").click(function () {
+    $(this).closest(".main-nav__item").find(".mob-dropdown-menu").slideToggle();
+    $(this).find("i").toggleClass("fa-chevron-up");
   });
 });
